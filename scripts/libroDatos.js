004cd9473d7df071a58e06bac554335fc4df6779
luciagarat <88253288+luciagarat@users.noreply.github.com>
2021-09-09 10:07:36 -0300

    Add files via upload

---
+++ scripts/libroDatos.js
@@ -0,0 +1,40 @@
+var arrayDatos = [];
+function showLibro(array) {
+    let libro = "";
+    let fotos = "";
+    libro += `
+    <h2> <strong>${array.name} </strong></h2><br>
+    <p> ${array.description} </p><br><p>${array.category}</p>
+    <div>${fotos}</div><br>
+    <p>Precio: ${array.cost} ${array.currency} </p>
+    `
+    fotos += `
+    <img class="img" src="img/${array.name}/1.jfif>" 
+    <img class="img" src="img/${array.name}/2.jfif>"
+    <img class="img" src="img/${array.name}/3.jfif>"
+    `
+
+
+    document.getElementById("listadoDatos").innerHTML += libro;
+    document.getElementById("listadoDatos").innerHTML += fotos;
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(LIBROS_DATOS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            arrayDatos = resultObj.data;
+            
+            for (let i = 0; i < arrayDatos.length; i++) {
+                console.log(localStorage.getItem("Id"), " ", arrayDatos[i].id)
+                if(arrayDatos[i].id=== localStorage.getItem("Id")){
+                   array = arrayDatos;
+                   console.log("hola")
+                    showLibro(arrayDatos);
+                }
+            }
+        }
+    })
+});
